a7c6e90def5c7e93b193ab26294b6b3f7b29460a
Suryazi Samiullah Khan <git@reseller.suryazi.com>
2014-08-07 13:25:30 +0300

    Change field name to ename in Emp

--- grails-app/domain/com/hrms/Emp.groovy
+++ grails-app/domain/com/hrms/Emp.groovy
@@ -3,12 +3,12 @@ package com.hrms
 class Emp {
 
 	String num
-	String name
+	String ename
 	String aname
 
     static constraints = {
     	num unique:true, blank:false
-    	name blank:false
+    	ename blank:false
     	aname blank:false
     }
 }
